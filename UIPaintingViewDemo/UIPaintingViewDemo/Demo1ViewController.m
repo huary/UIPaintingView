001c06f3ebbd868b520bba0748f3bf25dc68634b
yuan <yuan@yuandemacbook-pro.local>
2018-07-03 19:31:53 +0800

    修改测试案例

--- UIPaintingViewDemo/UIPaintingViewDemo/Demo1ViewController.m
+++ UIPaintingViewDemo/UIPaintingViewDemo/Demo1ViewController.m
@@ -1,26 +1,23 @@
 //
-//  Demo2ViewController.m
+//  Demo1ViewController.m
 //  UIPaintingViewDemo
 //
 //  Created by yuan on 2018/7/3.
 //  Copyright © 2018年 yuan. All rights reserved.
 //
 
-#import "Demo2ViewController.h"
+#import "Demo1ViewController.h"
 #import "UIPaintingView.h"
 
 
-@interface Demo2ViewController ()<UIPaintViewDelegate>
+@interface Demo1ViewController ()<UIPaintViewDelegate>
 
+/** 注释 */
 @property (nonatomic, strong) UIPaintingView *paintingView;
-
-@property (nonatomic, strong) UIPaintingView *playbackView;
-
-
 
 @end
 
-@implementation Demo2ViewController
+@implementation Demo1ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -40,65 +37,41 @@
     return button;
 }
 
--(UIPaintingView*)playbackView
-{
-    if (_playbackView == nil) {
-//        _playbackView = [[UIPaintingView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.paintingView.frame) + 10, SCREEN_WIDTH, self.paintingView.frame.size.height)];
-        _playbackView = [[UIPaintingView alloc] initWithFrame:self.paintingView.bounds];
-//        _playbackView = [[UIPaintingView alloc] initWithFrame:self.paintingView.frame];
-        _playbackView.backgroundColor = LIGHT_GRAY_COLOR;
-        _playbackView.brushWidth = 3.0;
-        _playbackView.brushColor = PURPLE_COLOR;
-        _playbackView.touchPaintEnabled = NO;
-        [self.paintingView addSubview:_playbackView];
-//        [self.view addSubview:_playbackView];
-    }
-    return _playbackView;
-}
-
 -(void)_btnAction:(UIButton*)sender
 {
     switch (sender.tag) {
         case 1:
         {
-            self.playbackView.paintEvent = [[NSPaintManager sharePaintManager] cacheForEventId:self.paintingView.paintEvent.eventId];
-            dispatch_after_in_main_queue(0.3, ^{
-                [self.playbackView playBack:YES];
-            });
+            [self.paintingView redo];
             break;
         }
         case 2: {
-            [self.playbackView removeFromSuperview];
-            self.playbackView = nil;
-            //1、测试方法1
-            [self.paintingView erase];
-            self.paintingView.brushColor = ORANGE_COLOR;
-            self.paintingView.brushWidth = 8.0;
-            
-            //2、测试方法2
-//            NSPaintStroke *last = [self.paintingView.paintEvent lastPaintStroke];
-//            [self.paintingView renderWithStroke:last];
-            
-            //3、测试方法3
-//            GLLinePoint *first =[[GLLinePoint alloc] initWithPoint:CGPointMake(0, 0) lineWidth:10];
-//            GLLinePoint *end = [[GLLinePoint alloc] initWithPoint:CGPointMake(100, 100) lineWidth:20];
-//            [self.paintingView renderLineFromPoint:first toPoint:end present:YES];
-            //4、测试方法4
-//            self.paintingView.frame = CGRectMake(0, 0, SCREEN_WIDTH, self.paintingView.frame.size.height + 64);
+            [self.paintingView undo];
+            //            [self.paintingView undoFaster];
             break;
         }
         case 3: {
-
+            //            CGRect rect = [self.cropView cropRectForType:NSCropRectTypeIn];
+            //            [self.paintingView eraseInFrame:rect];
+            [self.paintingView erase];
             break;
         }
         case 4: {
-
+            [self.paintingView deletePaint];
+            self.paintingView.paintEvent = [[NSPaintManager sharePaintManager] cacheForNewEvent];
             break;
         }
         case 5: {
+            //            self.paintingView.playRatio = 5.0;
+            [self.paintingView playBack:YES];
+            //            sender.tag = 6;
+            //            [sender setTitle:@"stop" forState:UIControlStateNormal];
             break;
         }
         case 6: {
+            [self.paintingView playBack:NO];
+            sender.tag = 5;
+            [sender setTitle:@"play" forState:UIControlStateNormal];
             break;
         }
         case 100: {
@@ -112,16 +85,11 @@
 -(void)_setupChildView
 {
     self.view.backgroundColor = WHITE_COLOR;
-
     CGFloat functionBtnHeight = 40;
     
     UIButton *closeBtn = [self  _createButton:@"关闭" frame:CGRectMake(0, 0, 60, 64) tag:100];
-
     
-    CGFloat Vspace = 10;
-    CGFloat paintingViewH = (SCREEN_HEIGHT - closeBtn.bounds.size.height - 2 * functionBtnHeight - Vspace)/2;
-    
-    self.paintingView = [[UIPaintingView alloc] initWithFrame:CGRectMake(0, closeBtn.bounds.size.height, SCREEN_WIDTH, paintingViewH)];
+    self.paintingView = [[UIPaintingView alloc] initWithFrame:CGRectMake(0, closeBtn.bounds.size.height, SCREEN_WIDTH, SCREEN_HEIGHT -closeBtn.bounds.size.height - 3 * functionBtnHeight)];
     self.paintingView.backgroundColor = LIGHT_GRAY_COLOR;
     self.paintingView.brushWidth = 3.0;
     self.paintingView.brushColor = RED_COLOR;
@@ -130,21 +98,28 @@
     self.paintingView.paintEvent = [[NSPaintManager sharePaintManager] cacheForNewEvent];
     [self.view addSubview:self.paintingView];
     
-    NSLog(@"paintingView=%@",self.paintingView)
-    
     CGFloat space = 15;
-    NSInteger cnt = 2;
+    NSInteger cnt = 5;
     CGFloat w = (SCREEN_WIDTH - (cnt + 1) * space)/cnt;
     
     CGFloat x = space;
-    CGFloat y = CGRectGetMaxY(self.paintingView.frame) + paintingViewH + Vspace + (SCREEN_HEIGHT - CGRectGetMaxY(self.paintingView.frame) - paintingViewH - Vspace - functionBtnHeight)/2;
-    
-    UIButton *btn = [self _createButton:@"play" frame:CGRectMake(x, y, w, functionBtnHeight) tag:1];
+    CGFloat y = CGRectGetMaxY(self.paintingView.frame) + 40;
+    UIButton *btn = [self _createButton:@"Redo" frame:CGRectMake(x, y, w, functionBtnHeight) tag:1];
     
     x = CGRectGetMaxX(btn.frame) + space;
-    btn = [self _createButton:@"remove" frame:CGRectMake(x, y, w, functionBtnHeight) tag:2];
-
-
+    btn = [self _createButton:@"Undo" frame:CGRectMake(x, y, w, functionBtnHeight) tag:2];
+    
+    x = CGRectGetMaxX(btn.frame) + space;
+    btn = [self _createButton:@"Erase" frame:CGRectMake(x, y, w, functionBtnHeight) tag:3];
+    
+    x = CGRectGetMaxX(btn.frame) + space;
+    btn = [self _createButton:@"delete" frame:CGRectMake(x, y, w, functionBtnHeight) tag:4];
+    
+    x = CGRectGetMaxX(btn.frame) + space;
+    btn = [self _createButton:@"play" frame:CGRectMake(x, y, w, functionBtnHeight) tag:5];
+    
+    
+    //    self.cropView = [[YZHUICropView alloc] initWithCropOverView:self.paintingView];
 }
 
 #pragma  mark
@@ -152,7 +127,9 @@
 {
     CGPoint currLoc = [touch locationInView:paintingView];
     
-//    NSLog(@"start.loc=%@",NSStringFromCGPoint(currLoc));
+    NSLog(@"start.loc=%@",NSStringFromCGPoint(currLoc));
+    
+    //    NSPaintPoint *paintPoint = [[NSPaintPoint alloc] initWithPoint:currLoc status:NSPaintStatusBegan lineWidth:self.paintingView.brushWidth];
     
     NSTimeInterval timeInterval = [[NSDate date] timeIntervalSince1970];
     NSPaintPoint *paintPoint = [[NSPaintPoint alloc] initWithPoint:currLoc pressure:0 status:NSPaintStatusBegan timeInterval:timeInterval];
@@ -170,6 +147,7 @@
 {
     CGPoint currLoc = [touch locationInView:paintingView];
     
+    //    NSPaintPoint *paintPoint = [[NSPaintPoint alloc] initWithPoint:currLoc status:NSPaintStatusMove lineWidth:self.paintingView.brushWidth];
     NSTimeInterval timeInterval = [[NSDate date] timeIntervalSince1970];
     NSPaintPoint *paintPoint = [[NSPaintPoint alloc] initWithPoint:currLoc pressure:0 status:NSPaintStatusMove timeInterval:timeInterval];
     
@@ -183,11 +161,12 @@
     CGPoint prevLoc = [touch previousLocationInView:paintingView];
     CGPoint currLoc = [touch locationInView:paintingView];
     
-//    NSLog(@"end.loc=%@",NSStringFromCGPoint(currLoc));
+    NSLog(@"end.loc=%@",NSStringFromCGPoint(currLoc));
     
     NSPaintPoint *last = [[self.paintingView.paintEvent.lastRenderStroke paintPoints] lastObject];
     if (CGPointEqualToPoint(prevLoc, last.point)) {
         
+        //        NSPaintPoint *paintPoint = [[NSPaintPoint alloc] initWithPoint:currLoc status:NSPaintStatusEnd lineWidth:self.paintingView.brushWidth];
         NSTimeInterval timeInterval = [[NSDate date] timeIntervalSince1970];
         NSPaintPoint *paintPoint = [[NSPaintPoint alloc] initWithPoint:currLoc pressure:0 status:NSPaintStatusEnd timeInterval:timeInterval];
         
@@ -201,5 +180,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
